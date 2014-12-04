91d9a3b283f2aecdd205404bc72d9a36569cd714
Bernard Chu <bernard.chu@yahoo.com>
2014-12-04 14:52:42 -0800

    Installed passport and passport-google and required them in server. Added oauth settings file and info for Google.

---
+++ server/oauth.js
@@ -0,0 +1,7 @@
+var ids = {
+  google: {
+   returnURL: 'http://cb-stage.azurewebsites.net/auth/google/callback',
+   realm: 'http://cb-stage.azurewebsites.net'
+  }
+};
+module.exports = ids;
--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var app = express();
-
+var config = require('./oauth.js')
+var passport = require('passport')
+var GoogleStrategy = require('passport-google').Strategy;
 
 
 
