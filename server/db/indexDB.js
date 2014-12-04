b6147a01846b14e75982ac0329c72fc3b4aa7a86
Issac <issac@Issacs-MacBook-Air.local>
2014-12-04 13:36:56 -0800

    added an sql file and sequelize tables creation

---
+++ server/db/indexDB.js
@@ -0,0 +1,30 @@
+var Sequelize = require('Sequelize');
+var sequelize = new Sequelize('', 'root', '', {
+	dialect: "mysql",
+	port: 3000,
+});
+
+var User = sequelize.define('user', {
+  username: Sequelize.STRING
+});
+
+var Category = sequelize.define('category', {
+  category: Sequelize.STRING
+});
+
+var Question = sequelize.define('question', {
+  question: Sequelize.STRING,
+  answer: Sequelize.STRING,
+  difficulty: Sequelize.INTEGER
+});
+
+User.hasMany(Question);
+Category.hasMany(Question);
+
+User.sync();
+Category.sync();
+Question.sync();
+
+exports.User = User;
+exports.Category = Category;
+exports.Question = Question;
