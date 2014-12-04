2028f726f7f8a11ea8c0543ba62ef93ded3ef37f
zindlerb <zindlerb@gmail.com>
2014-12-04 16:12:43 -0600

    very basic slideshow

--- public/questions/questions.js
+++ public/questions/questions.js
@@ -6,7 +6,9 @@ angular.module('copperBobcat.questions', [])
 }).factory('Questions', function() {
   //Linked list?
   var questions = {};
-  questions.list = [{question:"\nfunction add(a, b) {\n return a + b \n} \nadd(12, 3)", answer: '15'}];
+  questions.list = [{question:"\nfunction add(a, b) {\n return a + b \n} \nadd(12, 3)", answer: '15'},
+                    {question:"\nfunction subtract(a, b) {\n return a - b \n} \nsubtract(12, 3)", answer: '9'},
+                    {question:"\nfunction divide(a, b) {\n return a / b \n} \ndivide(12, 3)", answer: '4'}];
   questions.index = 0;
   questions.isAnswered = false;
   
