a1ac770ef27e471f28f7e37bb46a2a5d5962daf3
Rick Takes <rick@theparkmart.com>
2014-12-04 14:52:34 -0800

    Added database schema in sequelize

--- server/server.js
+++ server/server.js
@@ -1,29 +1,8 @@
 var express = require('express');
-var Sequelize = require('sequelize')
-  , sequelize = new Sequelize('copperdb', 'copperdb', 'Copper2014!');
-
-
-
-// var User = sequelize.define('User', {
-//   username: Sequelize.STRING,
-//   birthday: Sequelize.DATE
-// });
-
-// return sequelize.sync().then(function() {
-//   return User.create({
-//     username: 'sdepold',
-//     birthday: new Date(1986, 06, 28)
-//   }).then(function(sdepold) {
-//     console.log(sdepold.values)
-//   });
-// });
-
-
-
+var db = require('./db');
 var app = express();
 
 var port = process.env.PORT || 3000;
-
 app.use(express.static(__dirname + '../../public'));
 
 
