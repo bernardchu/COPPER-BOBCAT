9fdb89809cdf58448a5719538d3cfa09ca2ae2c1
Rick Takes <rick@theparkmart.com>
2014-12-04 14:40:54 -0800

    DB prep

--- server/server.js
+++ server/server.js
@@ -1,8 +1,26 @@
 var express = require('express');
+var Sequelize = require('sequelize')
+  , sequelize = new Sequelize('copperdb', 'copperdb', 'Copper2014!');
+
+
+
+// var User = sequelize.define('User', {
+//   username: Sequelize.STRING,
+//   birthday: Sequelize.DATE
+// });
+
+// return sequelize.sync().then(function() {
+//   return User.create({
+//     username: 'sdepold',
+//     birthday: new Date(1986, 06, 28)
+//   }).then(function(sdepold) {
+//     console.log(sdepold.values)
+//   });
+// });
+
+
+
 var app = express();
-
-
-
 
 var port = process.env.PORT || 3000;
 
