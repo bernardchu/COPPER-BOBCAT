e2a3144ad881dcf3a1b0acf4e967832f2fe7a53e
Rick Takes <rick@theparkmart.com>
2014-12-04 17:30:49 -0800

    wipe db

--- server/db/indexDB.js
+++ server/db/indexDB.js
@@ -4,22 +4,22 @@ var sequelize = new Sequelize('copperdb', 'bcb996835c867c', 'ad7bf91f', {
   host: 'us-cdbr-azure-west-a.cloudapp.net'
 });
 
-var User = sequelize.define('user', {
-  username: Sequelize.STRING
-});
+// var User = sequelize.define('user', {
+//   username: Sequelize.STRING
+// });
 
-var Category = sequelize.define('category', {
-  category: Sequelize.STRING
-});
+// var Category = sequelize.define('category', {
+//   category: Sequelize.STRING
+// });
 
-var Question = sequelize.define('question', {
-  question: Sequelize.STRING,
-  answer: Sequelize.STRING,
-  difficulty: Sequelize.INTEGER
-});
+// var Question = sequelize.define('question', {
+//   question: Sequelize.STRING,
+//   answer: Sequelize.STRING,
+//   difficulty: Sequelize.INTEGER
+// });
 
-User.hasMany(Question);
-Category.hasMany(Question);
+// User.hasMany(Question);
+// Category.hasMany(Question);
 
 // User.sync();
 // Category.sync();
