24a90dd838176e5971204b1edf295fec1f4cc711
Issac <issac@Issacs-MacBook-Air.local>
2014-12-04 19:43:34 -0800

    rid bugs in controllers

--- server/controllers/controllers.js
+++ server/controllers/controllers.js
@@ -1,9 +1,3 @@
-var db = require('../db');
-
-module.exports = {
-  messages: {
-    get: function (req, res) {
-      db.Message.findAll({include: [db.User]})
 var db = require('../db/indexDB.js');
 // var bluebird = require('bluebird');
 
@@ -17,28 +11,19 @@ module.exports = {
           res.json(results);
         });
     },
-    post: function (req, res) {
-      db.User.findOrCreate({where: {username: req.body.username}})
-        .complete(function(err, results){
-          db.Message.create({
-            userid: results[0].dataValues.id,
-            text: req.body.message,
-            roomname: req.body.roomname
-          }).complete(function(err, results){
-            res.sendStatus(201);
-          });
-        });
-    }
     // post: function (req, res) {
     //   db.User.findOrCreate({where: {username: req.body.username}})
     //     .complete(function(err, results){
-    //       db.Question.create({
-           
+    //       db.Message.create({
+    //         userid: results[0].dataValues.id,
+    //         text: req.body.message,
+    //         roomname: req.body.roomname
     //       }).complete(function(err, results){
     //         res.sendStatus(201);
     //       });
     //     });
     // }
+   
   },
 
   users: {
