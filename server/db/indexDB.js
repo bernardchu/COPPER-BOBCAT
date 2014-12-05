fdbcfe0f63f09e668979530b734b36e4e0ea4bf9
Rick Takes <rick@theparkmart.com>
2014-12-05 15:40:10 -0800

    Added db utility functions

--- server/db/indexDB.js
+++ server/db/indexDB.js
@@ -156,18 +156,6 @@ var updateDb = {
 exports.queryDb = queryDb;
 exports.updateDb = updateDb;
 
-// var questions = [
-//   ["function test() {console.log(a); console.log(foo()); var a = 1; function foo() { \nreturn 2;}}\ntest()", "Logs: undefined and 2"]
-// ];
-// for(var i=0; i<questions.length; i++){
-//   updateDb.addQuestion(questions[i][0], questions[i][1], '1');
-// }
-
-  // queryDb.getCategories(function(data){
-  //   console.log(data);
-  // });
-
-
 }; //TEMP DB
 
 tempDB();
