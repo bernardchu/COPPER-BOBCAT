f4ff3dc592c88caafa26c14a1db42ecc7ce02441
Rick Takes <rick@theparkmart.com>
2014-12-04 19:10:39 -0800

    writing db setup

--- server/db/indexDB.js
+++ server/db/indexDB.js
@@ -32,12 +32,14 @@ Category.hasMany(Question, { as: 'Category_Question', foreignKey: 'question_Id' 
 // newQueston.save().success(function() {
 //   console.log('Question Added');
 // });
+
 // 
 //   {question:"\nfunction add(a, b) {\n return a + b \n} \nadd(12, 3)", answer: '15'},
   //{question:"\nfunction subtract(a, b) {\n return a - b \n} \nsubtract(12, 3)", answer: '9'},
   //{question:"\nfunction divide(a, b) {\n return a / b \n} \ndivide(12, 3)", answer: '4'}
 // 
 // 
+
 
 //Create tables if not exist 
 User.sync().then(function(){
@@ -49,6 +51,7 @@ Category.sync().then(function(){
 Question.sync().then(function(){
   console.log("Created Question");
 });
+
 
 exports.User = User;
 exports.Category = Category;
