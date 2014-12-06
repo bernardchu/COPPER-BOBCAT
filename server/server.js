fe8a1f51a0199b41f943ae5eb0cbf58baf1ece51
Rick Takes <rick@theparkmart.com>
2014-12-06 15:47:40 -0800

    Fixed admin conflict issue

--- server/server.js
+++ server/server.js
@@ -75,6 +75,7 @@ app.get('/logout', function(req, res){
 
 
 //handling requests and DB queries
+//ensureAuthenticated, 
 app.get('/questions', ensureAuthenticated, function(req, res) {
 
   db.queryDb.getQuestions(function(data) {
