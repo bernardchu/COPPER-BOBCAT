54e5327e1a376ee41b6d4e06af49617606a605ea
Issac <issac@Issacs-MacBook-Air.local>
2014-12-06 16:54:47 -0800

    deleted unneeded files

--- server/auth/ensureAuthenticated.js
+++
@@ -1,7 +0,0 @@
-// helper function to test authentication
-module.exports = function(req, res, next) {
-  if (req.isAuthenticated()) { return next(); }
-  console.log('not authenticated!');
-  // res.redirect('/#/login');
-  res.send('Forbidden');
-}
