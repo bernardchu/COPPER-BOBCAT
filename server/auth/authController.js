730f8757053c96d641b5657da0e681ed16bbee07
Issac <issac@Issacs-MacBook-Air.local>
2014-12-06 17:18:49 -0800

    testing authentication

--- server/auth/authController.js
+++ server/auth/authController.js
@@ -7,6 +7,14 @@ module.exports = {
   },
 
   authenticateCB: function(req, res) {
-    res.redirect('/account');
+    db.queryDb.getUsers(function (user) {
+          if(!user) {
+            db.updateDb.addUser({username: req.user.username, displayName: req.user.displayName, gitId: req.user.id});
+          }
+          req.session.regenerate(function() {
+            req.session.user = req.user;
+          });
+          res.redirect('/');
+      }, {where: {gitId: req.user.id}});
   }
 }
