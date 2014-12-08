3f1b7bf78aa189d3bc782822e442306ffe638ae9
Bernard Chu <bernard.chu@yahoo.com>
2014-12-08 15:50:14 -0800

    Added passport tutorial link in comments.

--- server/helpers/oauth.js
+++ server/helpers/oauth.js
@@ -2,6 +2,7 @@
   * Stores Oauth ID and secret information for whatever sites you want to use for authentication.
   * The callbackURL is given to each site when registering, so it's not recommended you change it without changing info on the provider as well.
   * The provider will redirect users to the callbackURL once authenticated, so there should be a route on the server corresponding to each one.
+  * Passport functionality was mostly based on this tutorial: {@link http://mherman.org/blog/2013/11/10/social-authentication-with-passport-dot-js/#.VIOPy6TF8Yd}
   */
 var ids = {
   github: {
