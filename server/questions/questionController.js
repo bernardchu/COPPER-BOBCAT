8ac8a8ce5bdf729028685e6ea7024be62ca6a4dd
Issac <issac@Issacs-MacBook-Air.local>
2014-12-08 10:32:40 -0800

    added comments in server code

--- server/questions/questionController.js
+++ server/questions/questionController.js
@@ -20,6 +20,14 @@ module.exports = {
     });
   },
 
+  modifyQuestion : function(req, res){
+    console.log(req.body);
+    db.updateDb.modifyQuestion(req.body.id, req.body.field, req.body.value, function(){
+      //send response
+      res.send(201);
+    });
+  },
+
   deleteQuestion : function(req, res){
     db.updateDb.deleteById(req.body.id, function() {
       //send response
