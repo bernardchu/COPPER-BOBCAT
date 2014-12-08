6be3b014138c2996622a8e9b48445d75ebab1a2f
Rick Takes <rick@theparkmart.com>
2014-12-08 13:54:34 -0800

    Added admin add and delete functionality

--- public/admin/admin.js
+++ public/admin/admin.js
@@ -1,25 +1,74 @@
-angular.module('copperBobcat.admin', [])
-.controller('AdminController', function ($scope, Admin, $http, $state) {
-  angular.extend($scope, Admin);
 
-   Admin.getQuestions()
-    .then(function(questions) {
-      $scope.adminData = questions;
+angular.module('copperBobcat.admin', ['datatables'])
+.controller('AdminController', function($scope, $http, $compile, DTOptionsBuilder, DTColumnBuilder) {
+    $scope.reloadData = function() {
+      console.log('reload');
+        $scope.dtOptions.reloadData();
+    };
+
+    $scope.question = {};
+
+    $scope.addQuestion = function(data) {
+
+        $http({
+            method: 'POST',
+            url: '/api/questions/addQuestion',
+            data: data
+          })
+          .then(function(res){
+            $scope.dtOptions.reloadData(); 
+          });
+    };
+
+
+    // $scope.editItem = function(id) {
+    //     // Edit some data and call server to make changes...
+    //     // Then reload the data so that DT is refreshed
+    //     console.log('Edit item' + id);
+        
+    // };
+
+    $scope.deleteItem = function(id) {
+        // Delete some data and call server to make changes...
+        // Then reload the data so that DT is refreshed
+        console.log('Delete Item ' + id);
+
+        $http({
+            method: 'POST',
+            url: '/api/questions/delete',
+            data: {'id' : id}
+          })
+          .then(function(res){
+            console.log(res);
+            $scope.dtOptions.reloadData(); 
+            $scope.$apply();
+          });
+    };
+
+    $scope.dtOptions = DTOptionsBuilder.fromSource('/api/questions/')
+      .withPaginationType('full_numbers')
+      .withOption('createdRow', function(row, data, dataIndex){
+        $compile(angular.element(row).contents())($scope);
     });
 
-}).factory('Admin', function($http) {
-  var getQuestions = function() {
-    return $http({
-      method: 'GET',
-      url: '/questions'
-    })
-    .then(function(res){
-      return res.data;
-    });
-  };
-
-  return {
-    getQuestions: getQuestions
-  };
-
+    $scope.dtColumns = [
+        DTColumnBuilder.newColumn('id').withTitle('ID'),
+        DTColumnBuilder.newColumn('question').withTitle('Question'),
+        DTColumnBuilder.newColumn('answer').withTitle('Answer'),
+        DTColumnBuilder.newColumn('difficulty').withTitle('Difficulty'),
+        DTColumnBuilder.newColumn('createdAt').withTitle('Created At'),
+        DTColumnBuilder.newColumn('updatedAt').withTitle('Updated At'),
+        DTColumnBuilder.newColumn('id').withTitle('Edit Options')
+        .renderWith(function(data, type, full, meta) {
+                return '<button class="btn btn-warning" ng-click="editItem('+ data +')">' +
+                    '   <i class="fa fa-edit"></i>' +
+                    '</button>&nbsp;' +
+                    '<button class="btn btn-danger" ng-click="deleteItem('+ data +')">' +
+                    '   <i class="fa fa-trash-o"></i>' +
+                    '</button>';
+            })
+       //$scope.$apply()
+    ];
 });
+
+
--- server/questions/questionController.js
+++ server/questions/questionController.js
@@ -6,10 +6,20 @@ var db = require('../db/indexDB.js');
   */
 module.exports = {
 	getQuestions: function(req, res) {
-		  db.queryDb.getQuestions(function(data) {
-		    if(data) {
-		      res.json(data);
-		    }
-		  });
-	}
+	  db.queryDb.getQuestions(function(data) {
+	    if(data) {
+	      res.json(data);
+	    }
+	  });
+	}, 
+
+  addQuestion : function(req, res){
+    db.updateDb.addQuestion(req.body.text, req.body.answer, req.body.difficulty);
+  },
+
+  deleteQuestion : function(req, res){
+    db.updateDb.deleteById(req.body.id, function(data) {
+
+    });
+  }
 }
