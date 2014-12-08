023375aec581025e03a897a40dd8d22084d3a916
Issac <issac@Issacs-MacBook-Air.local>
2014-12-08 10:32:40 -0800

    added comments in server code

--- server/account/accountRoutes.js
+++ server/account/accountRoutes.js
@@ -1,7 +1,10 @@
 var ensureAuthenticated = require('../helpers/ensureAuthenticated.js')
 var accountController = require('./accountController.js');
 
-
+/**
+	* @param {function} app - app is the argument passed in from server.js
+	* Catches routes, runs the ensureAuthenticated function, and delegates request to controller
+	*/
 module.exports = function (app) {
   app.get('/', ensureAuthenticated, accountController.account);
   app.get('/logout', accountController.logout);
--- server/questions/questionRoutes.js
+++ server/questions/questionRoutes.js
@@ -1,7 +1,10 @@
 var questionController = require('./questionController.js');
 var ensureAuthenticated = require('../helpers/ensureAuthenticated.js')
 
-
+/**
+	* @param {function} app - app is the argument passed in from server.js
+	* Catches '/' route, runs the ensureAuthenticated function, and delegates request to controller
+	*/
 module.exports = function (app) {
   app.get('/', ensureAuthenticated, questionController.getQuestions);
 };
